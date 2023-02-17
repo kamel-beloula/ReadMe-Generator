c6ccdbf3ca2abb55ec2e47e0531d17cd66b73cfe
kamel beloula <kamel.solo@live.com>
2023-02-17 18:48:56 +0000

    added an index.js file and the questions structure

---
+++ index.js
@@ -0,0 +1,48 @@
+// Questions for the user
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the project title?',
+    },
+     {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instruction for your project',
+    },
+   {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage instructions for your project',
+    },
+    {
+        type: 'input',
+        name: 'link',
+        message: 'Please provide a link to your deployed project'
+    },
+    {
+        type:'input'
+        name:
+        message:
+    },
+    {
+        type:'input'
+        name:
+        message:
+    },
+    {
+        type:'input'
+        name:
+        message:
+    },
+    {
+        type:'input'
+        name:
+        message:
+    },
+]
