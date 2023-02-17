cbc07e1e148793c39de6390fdd630526679822cf
kamel beloula <kamel.solo@live.com>
2023-02-17 19:18:12 +0000

    added choices for the license

--- index.js
+++ index.js
@@ -2,47 +2,67 @@
 const questions = [
     {
         type: 'input',
+        message: 'What is your project title?',
         name: 'title',
-        message: 'What is the project title?',
     },
      {
         type: 'input',
-        name: 'description',
         message: 'Please provide a description of your project',
+        name: 'description',
     },
     {
         type: 'input',
-        name: 'installation',
         message: 'Please provide installation instruction for your project',
+        name: 'installation',
     },
    {
         type: 'input',
-        name: 'usage',
         message: 'Please provide usage instructions for your project',
+        name: 'usage',
     },
     {
         type: 'input',
-        name: 'link',
-        message: 'Please provide a link to your deployed project'
+        message: 'Please provide a link to your deployed project',
+        name: 'url',
     },
     {
-        type:'input'
-        name:
-        message:
+        type: 'input',
+        message: 'Please add credits',
+        name: 'credits',
     },
     {
-        type:'input'
-        name:
-        message:
+        type: 'input',
+        message: 'Please provide test instructions for your project',
+        name:'test',
     },
     {
-        type:'input'
-        name:
-        message:
+        type: 'list',
+        message: 'choose a license for your project:',
+        name: 'license',
+        choices: [
+                    "Apache License 2.0",
+                    "GNU General Public License v3.0",
+                    "MIT License",
+                    "BSD T-Clause 'Simplified' License",
+                    "BSD 3-Clause 'New' or 'Revised' License",
+                    "Creative Commons Zero v1.0 Universal",
+                    "Eclipse Public License 2.0",
+                    "GNU Affero General Public License v2.1",
+                    "GNU General Public License v2.0",
+                    "GNU Lesser General Public License v2.1",
+                    "GNU Lesser General Public License v3.0",
+                    "Mozilla Public License 2.0",
+                    "The Unilicense",
+                ]
     },
     {
-        type:'input'
-        name:
-        message:
+        type:'input',
+        message:'what is your github username?',
+        name: 'github',
+    },
+    {
+        type:'input',
+        message:'what is your email address?',
+        name: 'email',
     },
 ]
