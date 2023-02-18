4f64045c4087a62361183f68534fa046df2e1b05
kamel beloula <kamel.solo@live.com>
2023-02-18 21:42:09 +0000

    added licenses badges

--- index.js
+++ index.js
@@ -76,3 +76,24 @@ inquirer
     fs.writeFile('readme-demo.md',`title is: ${response.title}`,(err) =>
     err ? console.error(err) : console.log('readme.md generated'));
 })
+
+const license = {
+    [
+            // licenses from GitHub
+            // details from //https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
+            { id: 'Apache 2.0', name: 'Apache License 2.0',                         badge: { message: 'Apache_2.0', colour: 'blue', url: 'https://opensource.org/licenses/Apache-2.0' } },
+            { id: 'GNU GPL v3', name: 'GNU General Public License v3.0',            badge: { message: 'GPLv3', colour: 'blue', url: 'https://www.gnu.org/licenses/gpl-3.0' } },
+            { id: 'MIT', name: 'MIT License',                                       badge: { message: 'MIT', colour: 'yellow', url: 'https://opensource.org/licenses/MIT' } },
+            { id: 'BSD 2-Clause', name: 'BSD 2-Clause "Simplified" License',        badge: { message: 'BSD_2--Clause', colour: 'orange', url: 'https://opensource.org/licenses/BSD-2-Clause' } },
+            { id: 'BSD 3-Clause', name: 'BSD 3-Clause "New" or "Revised" License',  badge: { message: 'BSD_3--Clause', colour: 'blue', url: 'https://opensource.org/licenses/BSD-3-Clause' } },
+            { id: 'Boost 1.0', name: 'Boost Software License 1.0',                  badge: { message: 'Boost_1.0', colour: 'lightblue', url: 'https://www.boost.org/LICENSE_1_0.txt' } },
+            { id: 'CC0 1.0', name: 'Creative Commons Zero v1.0 Universal',          badge: { message: 'CC0_1.0', colour: 'lightgrey', url: 'http://creativecommons.org/publicdomain/zero/1.0/' } },
+            { id: 'EPL 2.0', name: 'Eclipse Public License 2.0',                    badge: { message: 'EPL_2.0', colour: 'red', ur: 'https://opensource.org/licenses/EPL-2.0' } },
+            { id: 'GNU AGPL v3', name: 'GNU Affero General Public License v3.0',    badge: { message: 'AGPL_v3', colour: 'blue', url: 'https://www.gnu.org/licenses/agpl-3.0' } },
+            { id: 'GNU GPL v2', name: 'GNU General Public License v2.0',            badge: { message: 'GPL_v2', colour: 'blue', url: 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html' } },
+            { id: 'GNU LGPL v2.1', name: 'GNU Lesser General Public License v2.1',  badge: { message: 'LGPL_v2.1', colour: 'blue', url: 'https://www.gnu.org/licenses/lgpl-2.1' } },
+            { id: 'Mozilla Public License 2.0', name: 'Mozilla Public License 2.0', badge: { message: 'MPL_2.0', colour: 'brightgreen', url: 'https://opensource.org/licenses/MPL-2.0' } },
+            { id: 'The Unlicense', name: 'The Unlicense',                           badge: { message: 'Unlicense', colour: 'blue', url: 'https://unlicense.org/' } },
+            { name: noLicense }
+    ]
+}
