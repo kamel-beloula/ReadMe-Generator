79aae86f63a6721690f23869f45cd87738ebf4df
kamel beloula <kamel.solo@live.com>
2023-02-19 22:55:44 +0000

    added module exports and require

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const fs = require('fs');
 const generateReadme = require("./utils/generateReadme");
 const questions = require("./utils/questions");
 const licenses = require("./utils/licenses");
+
+inquirer.prompt(questions)
+.then((response) => {
+  fs.writeFile('readme-demo.md', generateReadme(),
+      err ? console.error(err) : console.log("Successfully wrote to DEMO-README.md")
+  )
+})
