0484eb840c0882608497411082c5c5bcd0f7c47c
kamel beloula <kamel.solo@live.com>
2023-02-19 21:57:33 +0000

    added a new file structure, a folder urils with questions, licenses and generateReasme files

---
+++ utils/licenses.js
@@ -0,0 +1,57 @@
+// licenses names and urls
+const licenses = [
+    {
+      name: "Apache License 2.0",
+      url: "https://www.apache.org/licenses/LICENSE-2.0.txt",
+    },
+    {
+      name: "GNU General Public License v3.0",
+      url: "https://www.gnu.org/licenses/gpl-3.0.en.html",
+    },
+    {
+      name: "MIT License",
+      url: "https://opensource.org/licenses/MIT",
+    },
+    {
+      name: "BSD T-Clause 'Simplified' License",
+      url: "https://opensource.org/licenses/BSD-2-Clause",
+    },
+    {
+      name: "BSD 3-Clause 'New' or 'Revised' License",
+      url: "https://spdx.org/licenses/BSD-3-Clause.html",
+    },
+    {
+      name: "Creative Commons Zero v1.0 Universal",
+      url: "https://choosealicense.com/licenses/cc0-1.0/",
+    },
+    {
+      name: "Eclipse Public License 2.0",
+      url: "https://www.eclipse.org/legal/epl-2.0/",
+    },
+    {
+      name: "GNU Affero General Public License v2.1",
+      url: "https://www.gnu.org/licenses/agpl-3.0.en.html",
+    },
+    {
+      name: "GNU General Public License v2.0",
+      url: "https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html",
+    },
+    {
+      name: "GNU Lesser General Public License v2.1",
+      url: "https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html",
+    },
+    {
+      name: "GNU Lesser General Public License v3.0",
+      url: "https://www.gnu.org/licenses/lgpl-3.0.en.html",
+    },
+    {
+      name: "Mozilla Public License 2.0",
+      url: "https://www.mozilla.org/en-US/MPL/2.0/",
+    },
+    {
+      name: "The Unilicense",
+      url: "https://unlicense.org/",
+    },
+  ];
+
+module.exports = licenses;
---
+++ utils/questions.js
@@ -0,0 +1,63 @@
+// questions array
+const questions= [
+{
+    type: 'input',
+    message: 'What is your project title?',
+    name: 'title',
+},
+ {
+    type: 'input',
+    message: 'Please provide a description of your project',
+    name: 'description',
+},
+{
+    type: 'input',
+    message: 'Please provide installation instruction for your project',
+    name: 'installation',
+},
+{
+    type: 'input',
+    message: 'Please provide usage instructions for your project',
+    name: 'usage',
+},
+{
+    type: 'input',
+    message: 'Please provide a link to your deployed project',
+    name: 'url',
+},
+{
+    type: 'input',
+    message: 'Please add credits',
+    name: 'credits',
+},
+{
+    type: 'input',
+    message: 'Please provide test instructions for your project',
+    name:'test',
+},
+{
+    type: 'list',
+    message: 'choose a license for your project:',
+    name: 'license',
+    choices: [
+        "MIT license", 
+        "Apache License 2.0", 
+        "GNU General Public License v3.0",
+        "Creative Commons Zero v1.0 Universal",
+        "Mozilla Public License 2.0",
+        "The Unlicense",
+    ]
+},
+{
+    type:'input',
+    message:'what is your github username?',
+    name: 'github',
+},
+{
+    type:'input',
+    message:'what is your email address?',
+    name: 'email',
+},
+];
+
+module.exports = questions;
