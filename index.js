f73ac3e42b7afb487541826345d07b3ece801141
kamel beloula <kamel.solo@live.com>
2023-02-20 20:19:40 +0000

    added new file structure and a simpler mardsown function

--- index.js
+++ index.js
@@ -1,12 +1,62 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateReadme = require("./utils/generateReadme");
 const questions = require("./utils/questions");
 const licenses = require("./utils/licenses");
 
 inquirer.prompt(questions)
 .then((response) => {
-  fs.writeFile('readme-demo.md', generateReadme(),
-      err ? console.error(err) : console.log("Successfully wrote to DEMO-README.md")
-  )
+  fs.writeFile('readme-demo.md',
+  
+
+  `# ${response.title} ![GitHub license](https://img.shields.io/badge/license-${response.license}-blue.svg)
+
+
+  ##Description
+  ${response.description}
+
+
+  ##table of contenet
+
+  *[Installation](#installation)
+  *[Usage](#usage)
+  *[Link to deployed app](#link to deployed app)
+  *[Credits](#credits)
+  *[Test](#test)
+  *[License](#license)
+  *[Question](#installation)
+  
+  
+  ## Installation
+  ${response.installation}
+  
+  
+  ## Usage 
+  ${response.usage}
+  
+  
+  ##Link to deployed all
+  ${licenses.link}
+  
+  
+  ## Credits
+  ${response.credits}
+  
+  
+  ## Tests
+  ${response.tests}
+  
+  
+  ## License
+  Copyright (c) [2020]
+  The license is ${response.license}. 
+  Read more about it at ${licenses.Link}.
+  
+  
+  
+  ## Questions
+  If you have any additional questions please contact me at ${response.email}.
+  GitHub: https://github.com/${response.github}
+  `,(err) =>
+  err ? console.error(err) : console.log('User\'s info logged!')
+)
 })
